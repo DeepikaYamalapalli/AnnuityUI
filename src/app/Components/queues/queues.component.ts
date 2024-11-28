0d1b63391db59a3e523ebe38ca5e0348232fcbbc
Jagadeesh Arnepalli <jagadeesharnepalli@gmail.com>
2024-11-27 22:00:04 -0600

    Added Auth0 chnages

---
+++ src/app/Components/queues/queues.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-queues',
+  templateUrl: './queues.component.html',
+  styleUrl: './queues.component.css'
+})
+export class QueuesComponent {
+
+}
